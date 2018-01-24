a0b6f53c3116ca9d42718d8833f70bf3835c7c42
Valeriy <Kovshische@gmail.com>
2018-01-24 17:30:48 +0200

    Add loginTest

--- src/test/java/ua/org/mwdn/ExampleTest.java
+++ src/test/java/ua/org/mwdn/ExampleTest.java
@@ -7,16 +7,20 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import ua.org.mwdn.pages.LoginPage;
 
 public class ExampleTest {
 
     public static WebDriver driver;
 
+    //Pages
+    public static LoginPage loginPage;
+
     //private static final String DRIVER_HOME_DIRECTORY = "D:\\JavaExamples\\firsttest\\src\\main\\resources\\chromedriver_win32\\chromedriver.exe";
     private static final String DRIVER_HOME_DIRECTORY = "C:\\qaAutomation\\firsttest\\src\\main\\resources\\chromedriver_win32\\chromedriver.exe";
 
     //Loin credentials:
-    private static final String RIGHT_LOGIN = "admin@iroofing.org";
+    private static final String RIGHT_EMAIL = "admin@iroofing.org";
     private static final String PASSWORD = "aPPlicasa1981aPPlosophy";
     private static final String INCORRECT_PASSWORD = "777";
 
@@ -24,7 +28,11 @@ public class ExampleTest {
     public void setDriver(){
         System.setProperty("webdriver.chrome.driver",DRIVER_HOME_DIRECTORY);
         driver = new ChromeDriver();
+
+        LoginPage loginPage = new LoginPage(driver);
     }
+
+
 
 /*
     @Test
@@ -43,6 +51,9 @@ public class ExampleTest {
     public void loginTest(){
        // driver.get("http://google.com");
         driver.get("http://ec2-34-198-2-13.compute-1.amazonaws.com/");
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.visibilityOf(loginPage.loginField));
+        loginPage.loginField.sendKeys(RIGHT_EMAIL);
     }
 
 }
--- src/test/java/ua/org/mwdn/pages/LoginPage.java
+++ src/test/java/ua/org/mwdn/pages/LoginPage.java
@@ -1,15 +1,26 @@
 package ua.org.mwdn.pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import javax.xml.xpath.XPath;
 
 public class LoginPage {
+
+    public WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+
 /*
     @FindBy(id = "android.webkit.WebView")
     private WebElement endUserAgreementWebView;
  */
-    @FindBy(xpath = "")
-    private WebElement loginField;
+    @FindBy(xpath = "//*[@id=\"user_email\"]")
+    public WebElement loginField;
 }
