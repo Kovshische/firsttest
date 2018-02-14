8e1009ea86988fa7b158cc622c1b85bb57c5bef2
Valera <qa@test.com>
2018-02-14 19:10:39 +0200

    Add MenuHelper

---
+++ src/main/java/helpers/MenuHelper.java
@@ -0,0 +1,34 @@
+package helpers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import pagesObject.Menu;
+
+public class MenuHelper {
+
+
+    public WebDriver driver;
+
+    public MenuHelper(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+    public void isDashboardChosen() {
+        Menu menu = new Menu(driver);
+
+        final String attribute = menu.getDashboardActiveButton().getAttribute("class");
+        Assert.assertTrue(attribute.equals("active"));
+    }
+
+
+    public void isDashboardDisplayed() {
+        Menu menu = new Menu(driver);
+
+        Assert.assertTrue(menu.getDashboardButton().isDisplayed());
+    }
+
+}
